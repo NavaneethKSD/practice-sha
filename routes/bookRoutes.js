c33342622e39676d75690ced91385e119e589c4f
McQueenbot <mnavaneeth7@gmail.com>
2025-01-24 09:41:32 +0530

    fixed book Routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -6,6 +6,6 @@ router.post('/create', createBook);
 router.get('/read', getBooks);
 router.put('/update/:id', updateBook);
 router.delete('/delete/:id', deleteBook);
-router.get()
+
 
 module.exports = router;
